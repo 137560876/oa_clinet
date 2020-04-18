6ff4a28d443e0bad080302ba78a1ff90cf0ec068
137560876 <137560876@qq.com>
2020-04-18 16:28:55 +0800

    +1

--- src/pages/user/login/login.jsx
+++ src/pages/user/login/login.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 /**
  * @description: 登录的路由组件
  */
 
-export default class Login extends Component {
+export default class Login extends React.Component {
 
   render() {
     return (
